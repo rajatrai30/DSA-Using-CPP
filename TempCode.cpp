75df4618265590e0a2fe4f15a98fd7bb9c10c328
Rajat Rai <75417243+rajatrai30@users.noreply.github.com>
2024-04-23 19:12:48 +0530

    new prac push#2

--- TempCode.cpp
+++ TempCode.cpp
@@ -14,32 +14,55 @@
 // }
 
 // TIME PASS PRATICE STUFF
-#include<iostream>
-#include<vector>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// vector<int> SorterFunc(vector<int> a){
+//     int n = a.size();
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-1-i;j++){
+//             if(a[j]>a[j+1]){
+//                 swap(a[j],a[j+1]);
+//             }
+//         }
+//     }
+//     return a;
+// }
 
-vector<int> SorterFunc(vector<int> a){
-    int n = a.size();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-1-i;j++){
-            if(a[j]>a[j+1]){
-                swap(a[j],a[j+1]);
-            }
+// int main()
+// {
+//     vector<int> a, res;
+//     for(int num:a){
+//         cin>>num;
+//     }
+//     SorterFunc(a);
+//     for(int num:a){
+//         cout<<num<<" ";
+//     }
+//     return 0;
+// }
+
+// TIME PASS PRATICE STUFF 3
+#include <iostream>
+using namespace std;
+int ReturnLargest(int a[], int n){
+    int max = a[0];
+    for (int i=1;i<n;i++){
+        if(a[i]>max){
+            max = a[i];
         }
     }
-    return a;
+    return max;
 }
-
-int main()
-{
-    vector<int> a, res;
-
-    for(int num:a){
-        cin>>num;
+int main(){
+    // cout<<"Hello World";
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    SorterFunc(a);
-    for(int num:a){
-        cout<<num<<" ";
-    }
-    return 0;
+    int largest = ReturnLargest(arr,n);
+    cout<<largest;
+    return 0;   
 }
